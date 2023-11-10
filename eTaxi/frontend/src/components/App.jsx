8b793ffc60f44db51ded7a52780cbcce048f4719
Marina <marina.zilkina@gmail.com>
2023-11-10 11:50:24 +0200

    fresh start

--- eTaxi/frontend/src/components/App.jsx
+++ eTaxi/frontend/src/components/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import Logo from "./Logo";
-import YandexMaps from "./YandexMaps";
-import UselessButton from './UselessButton';
-import NewComponent from "./NewComponent.jsx";
+
 
 function App() {
   return (
     <div className="main-page">
       <header>
-            <Logo/>
           <h1> Header </h1>
-          <NewComponent/>
       </header>
         <main>
             <h2> Main</h2>
-            <YandexMaps/>
-            <UselessButton/>
         </main>
         <footer>
             <h2> Footer</h2>
