be33397dc410a9d74433c2a557e6074643e97d9e
Gitgeograph <Karaxanoval@mail.ru>
2023-11-12 10:36:55 +0500

    Поправил урл для картинки

--- eTaxi/eTaxi/urls.py
+++ eTaxi/eTaxi/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .yasg import urlpatterns as documentation_api
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
 ]
 
 urlpatterns += documentation_api
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
