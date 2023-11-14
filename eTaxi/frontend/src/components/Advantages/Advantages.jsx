14ae21e97e4a6c20b39970ddda36764ab5322b6b
Mariya Kochanova <balalum@gmail.com>
2023-11-14 18:11:24 +0400

    add subtitle

---
+++ eTaxi/frontend/src/components/Advantages/Advantages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Subtitle from '../Subtitle.jsx';
+
+function Advantages() {
+    return (
+        <section className='section'>
+            <div className='container'>
+                <Subtitle text="почему выбирают нас" />
+            </div>
+        </section>
+    )
+}
+
+export default Advantages;
