1840b86aedffa43e35cac0d477fc96e3890dd74c
Gitgeograph <Karaxanoval@mail.ru>
2023-11-15 12:49:46 +0500

    создал urls проекта

--- eTaxi/eTaxi/urls.py
+++ eTaxi/eTaxi/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .yasg import urlpatterns as documentation_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
+    path('api/v1/', include('backend.urls')),
     path('api/', include('frontend.urls')),
 ]
 
+urlpatterns += documentation_api
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
