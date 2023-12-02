f13a90ca8d281023b05ae2f724c47f7d9e19aac1
Gitgeograph <Karaxanoval@mail.ru>
2023-12-02 19:34:13 +0500

    добавил водителей в админку

--- eTaxi/backend/admin.py
+++ eTaxi/backend/admin.py
@@ -3,8 +3,13 @@ from .models import *
 
 # Register your models here.
 
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'comment', 'created_at')
+
+
 admin.site.register(City)
 admin.site.register(Car)
 admin.site.register(Office)
 admin.site.register(Feedback)
 admin.site.register(Employee)
+admin.site.register(Driver, DriverAdmin)
